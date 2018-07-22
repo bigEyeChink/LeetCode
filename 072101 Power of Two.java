316269e5f129ef9b04b6b73a0135103ba6da032b
BigEyeChink <li.8906@buckeyemail.osu.edu>
2018-07-21 22:20:14 -0400

    Power of two

--- 072101 Power of Two.java
+++ 072101 Power of Two.java
@@ -22,9 +22,18 @@
 // Input: 218
 // Output: false
 
+
+//Orginal 
+// 遇到这题的第一反应是原来写过的一个方法 利用递归 类似上课时候的 fastpower 
+// 但是感觉效率可能还是不高 继续分析 发现2 的倍数 不会出现0 结尾的 
+// 因为10 需要5 * 2 但是2的多少次方都不会5 结尾 
+// 因此选择用 2 4 6 8 的判断  到底是 %2 == 0 快 还是我的方法快  需要验证
+
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(  n == 1 ){
+        // basecase 出错  第一遍写成了 n == 0
+        // 不要相信的自己的直觉  再小的问题也要想到底 不要跳步骤
+        if( n == 1 ){
             return true;
         }
         else{
@@ -37,3 +46,26 @@ class Solution {
         
     }
 }
+
+// 最优解法
+// running time 一样  但是这种方法应该还是少一步 2进制到10进制的转化 对于底层来说 
+// 这种方法还是更快一点
+
+
+
+// 二进制中 2 的n次方 为 1 10 100 1000 10000 ..... 
+// bitCount 输出 n转化为二进制数字之后 1 的个数
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        return (Integer.bitCount(n)==1 && n>0);
+        
+        
+    }
+}
+
+
+// 小结:
+// 根据二应该能联想二进制的特殊性 
+// 注意积累类似知识  多利用底层
+// 写代码不要省力气 验证到底再写
